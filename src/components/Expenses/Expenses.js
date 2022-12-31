b23a6ffa1e61309ffbb4ccb2107815bb0307f787
houssamelm <houssamelmasoudi@gmail.com>
2022-12-31 17:19:47 +0100

    added a chart and finalizing edits

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,26 @@
 import ExpenseItem from './ExpenseItem.js'
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter.js'
+import ExpensesList from './ExpensesList.js'
+import ExpensesChart from './ExpensesChart.js'
+import { useState } from 'react'
 
 const Expenses = (props) => {
+
+    const [filter, setFilter] = useState('2022')
+
+    const handleFilter = (selectedfilter) => {
+        setFilter(selectedfilter)
+    }
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filter
+    })
+
     return (
         <Card className="expenses">
-            {props.items.map((expense) =>
-            (
-                <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id}/>
-            ))}
+            <ExpensesFilter selected={filter} onSelect={handleFilter} />
+            <ExpensesChart expenses={filteredExpenses}/>
+            <ExpensesList items={filteredExpenses}/>
         </Card>
     )
 }
